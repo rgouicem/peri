1d5decaed033b3a8a16f3eba171c363aae74142f
Rehda Gouicem [m1] <gouicem@mahler.soc.lip6.fr>
2015-03-06 15:03:00 +0100

    driver works

--- lab2/e1/ledbtnC1J1A.c
+++ lab2/e1/ledbtnC1J1A.c
@@ -3,6 +3,8 @@
 #include <linux/fs.h>
 #include <asm/io.h>
 #include <mach/platform.h>
+#include <asm/uaccess.h>
+#include <linux/slab.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("maxime.bittan@gmail.com and redha.gouicem@gmail.com, 2015");
@@ -26,7 +28,8 @@ struct gpio_registers_t {
   uint32_t lvl[2];
 };
 
-static struct gpio_registers_t* gpio_p;
+static char* kbuf;
+static volatile struct gpio_registers_t* gpio_p;
 
 static int ledparam, btnparam;
 
@@ -39,7 +42,16 @@ struct file_operations fops = {.open = my_open_function,
 			       .read = my_read_function,
 			       .write = my_write_function,
 			       .release = my_release_function };
+static void SetGPIOFunction(int GPIO, int functionCode)
+{
+	int registerIndex = GPIO / 10;
+	int bit = (GPIO % 10) * 3;
 
+	unsigned oldValue = gpio_p->sel[registerIndex];
+	unsigned mask = 0b111 << bit;
+	printk("Changing function of GPIO%d from %x to %x\n", GPIO, (oldValue >> bit) & 0b111, functionCode);
+	gpio_p->sel[registerIndex] = (oldValue & ~mask) | ((functionCode << bit) & mask);
+}
 static int __init mon_module_init(void)
 {
   printk(KERN_DEBUG "Hello World !\n");
@@ -50,12 +62,21 @@ static int __init mon_module_init(void)
   //gpio_base_p = (uint32_t*)__io_address(GPIO_BASE);
   gpio_p = (struct gpio_registers_t*) __io_address(GPIO_BASE);
   // configuring pins
-  gpio_p->sel[GPIO_LED0/10] &= ~(0x7 << ((GPIO_LED0%10)*3)) | (2 << ((GPIO_LED0%10)*3));
-  gpio_p->sel[GPIO_LED1/10] &= ~(0x7 << ((GPIO_LED1%10)*3)) | (2 << ((GPIO_LED1%10)*3));
-  gpio_p->sel[GPIO_LED2/10] &= ~(0x7 << ((GPIO_LED2%10)*3)) | (2 << ((GPIO_LED2%10)*3));
-  gpio_p->sel[GPIO_LED3/10] &= ~(0x7 << ((GPIO_LED3%10)*3)) | (2 << ((GPIO_LED3%10)*3));
-  gpio_p->sel[GPIO_BTN0/10] &= ~(0x7 << ((GPIO_BTN0%10)*3)) | (1 << ((GPIO_BTN0%10)*3));
-  gpio_p->sel[GPIO_BTN1/10] &= ~(0x7 << ((GPIO_BTN1%10)*3)) | (1 << ((GPIO_BTN1%10)*3));
+  gpio_p->sel[GPIO_LED0/10] = (gpio_p->sel[GPIO_LED0/10] & ~(0x7 << ((GPIO_LED0%10)*3))) | (1 << ((GPIO_LED0%10)*3));
+  gpio_p->sel[GPIO_LED1/10] = (gpio_p->sel[GPIO_LED1/10] & ~(0x7 << ((GPIO_LED1%10)*3))) | (1 << ((GPIO_LED1%10)*3));
+  gpio_p->sel[GPIO_LED2/10] = (gpio_p->sel[GPIO_LED2/10] & ~(0x7 << ((GPIO_LED2%10)*3))) | (1 << ((GPIO_LED2%10)*3));
+  gpio_p->sel[GPIO_LED3/10] = (gpio_p->sel[GPIO_LED3/10] & ~(0x7 << ((GPIO_LED3%10)*3))) | (1 << ((GPIO_LED3%10)*3));
+  gpio_p->sel[GPIO_BTN0/10] = (gpio_p->sel[GPIO_BTN0/10] & ~(0x7 << ((GPIO_BTN0%10)*3))) | (0 << ((GPIO_BTN0%10)*3));
+  gpio_p->sel[GPIO_BTN1/10] = (gpio_p->sel[GPIO_BTN0/10] & ~(0x7 << ((GPIO_BTN1%10)*3))) | (0 << ((GPIO_BTN1%10)*3));
+  /* SetGPIOFunction(GPIO_LED0, 1); */
+  /* SetGPIOFunction(GPIO_LED1, 1); */
+  /* SetGPIOFunction(GPIO_LED2, 1); */
+  /* SetGPIOFunction(GPIO_LED3, 1) */;
+
+  if((kbuf = kmalloc(4, GFP_KERNEL)) == NULL) {
+    printk(KERN_ERR "kmalloc\n");
+    return -1;
+  }
   /* (*(gpio_base_p + (1*(GPIO_LED0/10))))=((*(gpio_base_p + (1*(GPIO_LED0/10)))) & ~(0x7 << ((GPIO_LED0%10)*3))) */
   /*   | (2 << ((GPIO_LED0%10)*3)) ; */
   /* (*(gpio_base_p + (1*(GPIO_LED1/10))))=((*(gpio_base_p + (1*(GPIO_LED1/10)))) & ~(0x7 << ((GPIO_LED1%10)*3))) */
@@ -75,6 +96,7 @@ static int __init mon_module_init(void)
 
 static void __exit mon_module_cleanup(void)
 {
+  kfree(kbuf);
   unregister_chrdev(0, "ledbtnC1J1A");
   printk(KERN_DEBUG "Goodbye World!\n");
 }
@@ -85,35 +107,55 @@ static int my_open_function (struct inode * inode, struct file * file){
 }
 
 static ssize_t my_read_function(struct file * file, char * buf, size_t count, loff_t * ppos){
+  char kbuf[2];
   printk(KERN_DEBUG "read()\n");
-  buf[0] = ((gpio_p->lvl[GPIO_BTN0/32] >> (GPIO_BTN0%32)) & 0x1) ? '1' : '0';
-  buf[1] = ((gpio_p->lvl[GPIO_BTN1/32] >> (GPIO_BTN1%32)) & 0x1) ? '1' : '0';
+  kbuf[0] = ((gpio_p->lvl[GPIO_BTN0/32] >> (GPIO_BTN0%32)) & 0x1) ? '1' : '0';
+  kbuf[1] = ((gpio_p->lvl[GPIO_BTN1/32] >> (GPIO_BTN1%32)) & 0x1) ? '1' : '0';
   //buf[0] = ((*(gpio_base_p + ((0x34/4)+GPIO_BTN0/32)))>>(GPIO_BTN0%32)) & 0x1 ? '1' : '0';
   //buf[1] = ((*(gpio_base_p + ((0x34/4)+GPIO_BTN1/32)))>>(GPIO_BTN1%32)) & 0x1 ? '1' : '0';
+  if(copy_to_user(buf, kbuf, count) > 0) {
+    printk(KERN_ERR "read() - error\n");
+    return -1;
+  }
   printk(KERN_DEBUG "read() - over\n");
   return 2;
 }
 
+static void SetGPIOOutputValue(int GPIO, bool outputValue)
+{
+	if (outputValue)
+		gpio_p->set[GPIO / 32] = (1 << (GPIO % 32));
+	else
+		gpio_p->clr[GPIO / 32] = (1 << (GPIO % 32));
+}
+
 static ssize_t my_write_function(struct file * file, const char * buf, size_t count, loff_t * ppos){
-  uint32_t *ad;
   printk(KERN_DEBUG "write()\n");
-  
-  if(buf[0] == '1')
-    gpio_p->set[GPIO_LED0/32] |= (0x1<<(GPIO_LED0%32));
+
+  if(copy_from_user(kbuf, buf, count) > 0) {
+    printk(KERN_ERR "write() - error\n");
+    return -1;
+  }
+  /* SetGPIOOutputValue(GPIO_LED0, 1); */
+  /* SetGPIOOutputValue(GPIO_LED1, 1); */
+  /* SetGPIOOutputValue(GPIO_LED2, 1); */
+  /* SetGPIOOutputValue(GPIO_LED3, 1); */
+  if(kbuf[0] == '1')
+    gpio_p->set[GPIO_LED0/32] = (0x1<<(GPIO_LED0%32));
   else
-    gpio_p->clr[GPIO_LED0/32] |= (0x1<<(GPIO_LED0%32));
-  if(buf[1] == '1')
-    gpio_p->set[GPIO_LED1/32] |= (0x1<<(GPIO_LED1%32));
+    gpio_p->clr[GPIO_LED0/32] = (0x1<<(GPIO_LED0%32));
+  if(kbuf[1] == '1')
+    gpio_p->set[GPIO_LED1/32] = (0x1<<(GPIO_LED1%32));
   else
-    gpio_p->clr[GPIO_LED1/32] |= (0x1<<(GPIO_LED1%32));
-  if(buf[2] == '1')
-    gpio_p->set[GPIO_LED2/32] |= (0x1<<(GPIO_LED2%32));
+    gpio_p->clr[GPIO_LED1/32] = (0x1<<(GPIO_LED1%32));
+  if(kbuf[2] == '1')
+    gpio_p->set[GPIO_LED2/32] = (0x1<<(GPIO_LED2%32));
   else
-    gpio_p->clr[GPIO_LED2/32] |= (0x1<<(GPIO_LED2%32));
-  if(buf[3] == '1')
-    gpio_p->set[GPIO_LED3/32] |= (0x1<<(GPIO_LED3%32));
+    gpio_p->clr[GPIO_LED2/32] = (0x1<<(GPIO_LED2%32));
+  if(kbuf[3] == '1')
+    gpio_p->set[GPIO_LED3/32] = (0x1<<(GPIO_LED3%32));
   else
-    gpio_p->clr[GPIO_LED3/32] |= (0x1<<(GPIO_LED3%32));
+    gpio_p->clr[GPIO_LED3/32] = (0x1<<(GPIO_LED3%32));
 
   /* ad = gpio_base_p + (0x1C/4)+ (GPIO_LED0/32); */
   /* *ad= buf[0]=='1' ? (*ad | (0x1<<(GPIO_LED0%32))) : (*ad & ~(0x1<<(GPIO_LED0%32))); */
@@ -124,11 +166,12 @@ static ssize_t my_write_function(struct file * file, const char * buf, size_t co
   /* ad = gpio_base_p + (0x1C/4)+ (GPIO_LED0/32); */
   /* *ad= buf[3]=='1' ? (*ad | (0x1<<(GPIO_LED3%32))) : (*ad & ~(0x1<<(GPIO_LED3%32))); */
 
+ 
   printk(KERN_DEBUG "write() - over\n");
   return 4;
 }
 
-static int my_release_function(struct inode * inode, struct file * file){
+static int my_release_function(struct inode * inode, struct file * file){ 
   printk(KERN_DEBUG "close()\n");
   return 0;
 }
